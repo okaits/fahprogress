78ebd50a6b42c8056c52a8820ac9c174a90abec8
itsukiokajima <itsukiokajima@gmail.com>
2021-02-13 09:50:26 +0900

    modified:   fahprogress.sh

--- fahprogress.sh
+++ fahprogress.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cat /var/lib/fahclient/log.txt | grep Completed | tac | nl | grep "  1" | cut -f 2 -d "(" | cut -f 1 -d ")"
+cat /var/lib/fahclient/log.txt | grep Completed | grep WU$1 | tac | nl | grep "  1" | cut -f 4 -d ":"
+cat /var/lib/fahclient/log.txt | grep Completed | grep WU$1 | tac | nl | grep "  1" | cut -f 2 -d "(" | cut -f 1 -d ")"
